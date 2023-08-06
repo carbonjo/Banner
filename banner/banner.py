1aeaa9d4076ef31def06d316c9bd9bd186334e78
Joaquin Carbonara <carbonjo@buffalostate.edu>
2023-08-06 12:17:47 -0400

    Fixed initiate_driver() so it works in Colab v2

--- banner/banner.py
+++ banner/banner.py
@@ -25,7 +25,7 @@ def initiate_driver():
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome('chromedriver', options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
     else:
         # Setup for running locally
         chrome_options = Options()
